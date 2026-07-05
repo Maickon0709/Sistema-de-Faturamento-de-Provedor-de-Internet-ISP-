dac6e691bcc4b22669bab84f40e452eaf462dbfb
Maickon <maickon0729@gmail.com>
2026-07-04 22:08:37 -0300

    subindo motor_calculo.py

---
+++ motor_calculo.py
@@ -0,0 +1,7 @@
+IMPOSTO_ALIQUOTA = 0.2725
+
+ TARIFAS = {
+     "R": {"td": 0.15, "ti": 0.10, "taxa_equip": 25.00},
+     "E": {"td": 0.20, "ti": 0.15, "taxa equip": 60.00},
+     "C": {"td": 0.28,}
+ }
